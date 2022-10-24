e5bdc752fafa3be5c3a27d708faf55ce3be42298
weiguo.ni <neefrankie@gmail.com>
2022-10-24 16:26:03 +0800

    doc

--- internal/auth/authorizer.go
+++ internal/auth/authorizer.go
@@ -7,12 +7,15 @@ import (
 	"google.golang.org/grpc/status"
 )
 
+// Authorizer wraps Casbin authorization.
 type Authorizer struct {
 	enforcer *casbin.Enforcer
 }
 
+// New creates a new instance of Authorizer.
+// The model and policy arguments are paths to the files where you defined the model.
 func New(model, policy string) *Authorizer {
-	enforcer, err := casbin.NewEnforcer(model, policy)
+	enforcer, err := casbin.NewEnforcer(model, policy, true)
 	if err != nil {
 		panic(err)
 	}
@@ -21,6 +24,9 @@ func New(model, policy string) *Authorizer {
 	}
 }
 
+// Authorize defers to Casbin's Enforce function to return whether the given
+// subject is permitted ron the given action on the given object based on the
+// model and policy you configure Casbin with.
 func (a *Authorizer) Authorize(subject, object, action string) error {
 	ok, err := a.enforcer.Enforce(subject, object, action)
 	if err != nil {
